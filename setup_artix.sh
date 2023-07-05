6c784196dc97103fbd8b71dccb0a31580e5dec81
Stormx920 <eriklrosebrock@tutanota.com>
2023-07-05 15:26:27 +0200

    updates

--- setup_artix.sh
+++ setup_artix.sh
@@ -25,25 +25,22 @@ while true; do
         if [ "$auto_prov_ans" = "y" ]; then
             wipefs -a $disk_chk
             parted -a optimal $disk_chk --script mklabel gpt
-            parted $disk_chk --script mkpart primary 1MiB 3MiB
+            parted $disk_chk --script mkpart primary 1MiB 257MiB
             parted $disk_chk --script name 1 grub
             parted $disk_chk --script set 1 bios_grub on
-            parted $disk_chk --script mkpart primary 3MiB 131MiB
-            parted $disk_chk --script name 2 boot
-            parted $disk_chk --script mkpart primary 131MiB 4227MiB
-            parted $disk_chk --script name 3 swap
-            parted $disk_chk --script -- mkpart primary 4227MiB -1
-            parted $disk_chk --script name 4 rootfs
-            parted $disk_chk --script set 2 boot on
+            parted $disk_chk --script mkpart primary 257MiB 4353MiB
+            parted $disk_chk --script name 2 swap
+            parted $disk_chk --script -- mkpart primary 4353MiB -1
+            parted $disk_chk --script name 3 artix
+            parted $disk_chk --script set 1 boot on
             part_1=("${disk_chk}1")
             part_2=("${disk_chk}2")
             part_3=("${disk_chk}3")
-            part_4=("${disk_chk}4")
-            mkfs.fat -F 32 $part_2
+            mkfs.fat -F 32 $part_1
             #mkfs.ext4 $part_2
-            mkfs.ext4 $part_4
-            mkswap $part_3
-            swapon $part_3
+            mkfs.ext4 $part_3
+            mkswap $part_2
+            swapon $part_2
             rm -rf devices
             clear
             sleep 2
